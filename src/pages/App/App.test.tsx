a34bef5077ecf121b8d937ada7a3dbe19d863624
deokgoo <kkddgg1001@gmail.com>
2021-01-02 23:13:26 +0900

    fix: template test

--- src/pages/App/App.test.tsx
+++ src/pages/App/App.test.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let container: null | HTMLElement = null;
+
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
 });
+
+afterEach(() => {
+  if(!container) return;
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+describe('test app', () => {
+  it('header title check', () => {
+    act(() => {
+      render(<App />, container);
+    });
+    expect(container?.textContent).toEqual(expect.stringContaining('Learn React'));
+  });
+})
